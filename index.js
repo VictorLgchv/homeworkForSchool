76b35c53c1e54a0ea209f7c58e6fb4c919980ba8
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-03-15 14:17:32 +0200

    add files

---
+++ index.js
@@ -0,0 +1,9 @@
+const myFirstModal = document.querySelector(".modal");
+const mySecondModal = document.querySelector(".second-modal modal");
+console.log(myFirstModal)
+// const myInput = document.getElementById('myInput')
+
+myFirstModal.addEventListener("click", function () {
+  // myInput.focus();
+  console.log("Hi")
+});

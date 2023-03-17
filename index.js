a9dd17bbeccdff78d65a5207abfef903bae066d7
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-03-17 12:03:32 +0200

    uppdated

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const myFirstModal = document.querySelector(".modal");
-const mySecondModal = document.querySelector(".second-modal modal");
-console.log(myFirstModal)
-// const myInput = document.getElementById('myInput')
+const refs = {
+  resultBtn: document.querySelector(".js-button"),
+  textarea: document.querySelector(".js-textarea"),
+  checkedFirstInput: document.querySelectorAll(".js-first-input"),
+  checkedSecondInput: document.querySelectorAll(".js-second-input"),
+};
+const { resultBtn, checkedFirstInput, checkedSecondInput, textarea } = refs;
+resultBtn.addEventListener("click", onBtnClick);
+function onBtnClick() {
+  verification(checkedFirstInput, checkedSecondInput);
+}
 
-myFirstModal.addEventListener("click", function () {
-  // myInput.focus();
-  console.log("Hi")
-});
+function verification(testOne, testTwo) {
+  console.log("this is form", testOne);
+  let result = 0;
+  if (testOne[2].checked) {
+    result += 1;
+  }
+  if (testTwo[2].checked) {
+    result += 1;
+  }
+  if (testTwo[0].checked) {
+    result += 1;
+  }
+  textarea.value = result;
+}
